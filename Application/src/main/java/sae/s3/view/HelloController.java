62016375ea1a8e60fbdca70ea848295d256a54c0
paulEspi117 <126152491+paulEspi117@users.noreply.github.com>
2023-11-14 10:29:59 +0100

    Update HelloController.java

--- Application/src/main/java/sae/s3/view/HelloController.java
+++ Application/src/main/java/sae/s3/view/HelloController.java
@@ -1,11 +1,21 @@
 package sae.s3.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Button historique;
+
+    @FXML
+    private Button parametrer;
+
+    @FXML
+    private Button salle;
 
     @FXML
     protected void onHelloButtonClick() {
