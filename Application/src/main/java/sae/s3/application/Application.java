bb35b10a41694629c096a818c447c08da2e13874
tristandlth <85364501+tristandlth@users.noreply.github.com>
2023-11-15 11:33:59 +0100

    Remise à zéro de l'environnement de développement

---
+++ Application/src/main/java/sae/s3/application/Application.java
@@ -0,0 +1,23 @@
+package sae.s3.application;
+
+/**
+ *
+ * Classe principale de lancement.
+ *
+ */
+
+import sae.s3.application.control.MainFrame;
+
+/**
+ * Démarrage de l'application DailyBank
+ *
+ */
+public class Application {
+
+    /**
+     *  Lancement de la fenêtre principale.
+     */
+    public static void main(String[] args) {
+        MainFrame.runApp();
+    }
+}
---
+++ Application/src/main/java/sae/s3/application/control/MainFrame.java
@@ -0,0 +1,31 @@
+package sae.s3.application.control;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import sae.s3.application.view.MainFrameController;
+
+import java.io.IOException;
+
+/**
+ * Classe de controleur de Dialogue de la fenêtre principale.
+ *
+ */
+public class MainFrame extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainFrameController.class.getResource("main-frame.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Méthode principale de lancement de l'application.
+     */
+    public static void runApp() {
+        Application.launch();
+    }
+}
