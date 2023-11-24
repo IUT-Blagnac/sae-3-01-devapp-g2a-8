7c78a7d21cf40bc93188405c2027da151023120f
Bastien PERRUFFEL <126145356+BastienPRFL@users.noreply.github.com>
2023-11-24 11:00:14 +0100

    Update DonneesCapteurs.py
    Log des données fonctionnel

--- Application/src/main/resources/sae/s3/application/python/DonneesCapteurs.py
+++ Application/src/main/resources/sae/s3/application/python/DonneesCapteurs.py
@@ -1,8 +1,36 @@
+import datetime
 import paho.mqtt.client as mqtt
 import os
 import json
 
 #The callback for when the client receives a CONNACK response from the server.
+
+def write_log(nomSalle, donneesHist):
+    fDest = os.open('donnees.json', os.O_WRONLY | os.O_CREAT | os.O_APPEND)
+    with open('historique.json', 'r') as fichier_json:
+        donnees_existantes = json.load(fichier_json)
+
+    if len(donnees_existantes) == 0:
+        donneesNvSalle = {nomSalle: donneesHist}
+        donnees_existantes.update(donneesNvSalle)
+
+    else:
+        existe = False
+        for cle, val in donnees_existantes.items():
+            if cle == nomSalle:
+                donnees_existantes[nomSalle].update(donneesHist)
+                existe = True
+                break
+
+        if not existe:
+            donneesNvSalle = {nomSalle: donneesHist}
+            donnees_existantes.update(donneesNvSalle)
+
+    with open('historique.json', 'w') as fichier_json:
+        json.dump(donnees_existantes, fichier_json, indent=4)
+
+    os.close(fDest)
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
@@ -17,34 +45,44 @@ def on_message(client, userdata, msg):
     data = json.loads(message) 
     fDest = os.open('donnees.json', os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
 
-    salle = data[1]["room"]
-    temp = data[0]["temperature"]
-    hum = data[0]["humidity"]
-    co2 = data[0]["co2"]
-    act = data[0]["activity"]
+    if "room" in data[1]:
+        salle = data[1]["room"]
+        temp = data[0]["temperature"]
+        hum = data[0]["humidity"]
+        co2 = data[0]["co2"]
+        act = data[0]["activity"]
 
-    os.write(fDest, b'{\n')
-    os.write(fDest, b'\t"Salle" : "')
-    os.write(fDest, str.encode(salle))
-    os.write(fDest, b'"')
-    os.write(fDest, b',\n\t"Temperature" : ')
-    os.write(fDest, str.encode(str(temp)))
-    os.write(fDest, b',\n\t"Humidite" : ')
-    os.write(fDest, str.encode(str(hum)))
-    os.write(fDest, b',\n\t"CO2" : ')
-    os.write(fDest, str.encode(str(co2)))
-    os.write(fDest, b',\n\t"Activite" : ')
-    os.write(fDest, str.encode(str(act)))
-    os.write(fDest, b'\n}')
+        os.write(fDest, b'{\n')
+        os.write(fDest, b'\t"Salle" : "')
+        os.write(fDest, str.encode(salle))
+        os.write(fDest, b'"')
+        os.write(fDest, b',\n\t"Temperature" : ')
+        os.write(fDest, str.encode(str(temp)))
+        os.write(fDest, b',\n\t"Humidite" : ')
+        os.write(fDest, str.encode(str(hum)))
+        os.write(fDest, b',\n\t"CO2" : ')
+        os.write(fDest, str.encode(str(co2)))
+        os.write(fDest, b',\n\t"Activite" : ')
+        os.write(fDest, str.encode(str(act)))
+        os.write(fDest, b'\n}')
+        os.close(fDest)
+        donneesHist = {
+            datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"): {
+                "Humidite": str(hum),
+                "CO2": str(co2),
+                "Activite": str(act),
+                "Alerte": "non"
+            }
+        }
 
-    print ("\n \n")
-    print("Salle : ", data[1]["room"])
-    print("Température : ", data[0]["temperature"])
-    print("Humidité : ", data[0]["humidity"])
-    print("CO2 : ", data[0]["co2"])
-    print("Activité : ", data[0]["activity"])
+        print ("\n \n")
+        print("Salle : ", data[1]["room"])
+        print("Température : ", data[0]["temperature"])
+        print("Humidité : ", data[0]["humidity"])
+        print("CO2 : ", data[0]["co2"])
+        print("Activité : ", data[0]["activity"])
 
-
+        write_log(salle, donneesHist)
 
 client = mqtt.Client()
 client.on_connect = on_connect
