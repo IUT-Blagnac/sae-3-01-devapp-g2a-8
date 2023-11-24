3a2d0e545f0322c916b4e642ebced9ba271a07f5
Bastien PERRUFFEL <126145356+BastienPRFL@users.noreply.github.com>
2023-11-24 16:04:59 +0100

    python update log

--- Application/src/main/resources/sae/s3/application/python/DonneesCapteurs.py
+++ Application/src/main/resources/sae/s3/application/python/DonneesCapteurs.py
@@ -6,30 +6,52 @@ import json
 #The callback for when the client receives a CONNACK response from the server.
 
 def write_log(nomSalle, donneesHist):
-    fDest = os.open('donnees.json', os.O_WRONLY | os.O_CREAT | os.O_APPEND)
-    with open('historique.json', 'r') as fichier_json:
-        donnees_existantes = json.load(fichier_json)
 
-    if len(donnees_existantes) == 0:
-        donneesNvSalle = {nomSalle: donneesHist}
-        donnees_existantes.update(donneesNvSalle)
+    if not os.path.exists("historique.json"):
+        date = list(donneesHist.keys())[0]
+
+        fDest = os.open('historique.json', os.O_RDWR | os.O_CREAT | os.O_APPEND)
+        os.write(fDest, b'{\n\t"')
+        os.write(fDest, str.encode(nomSalle))
+        os.write(fDest, b'": {\n')
+        os.write(fDest, b'\t\t"')
+        os.write(fDest, str.encode(date))
+        os.write(fDest, b'": {\n')
+        os.write(fDest, b'\t\t\t"Humidite": "')
+        os.write(fDest, str.encode(donneesHist[date]["Humidite"]))
+        os.write(fDest, b'",\n\t\t\t"CO2" : "')
+        os.write(fDest, str.encode(donneesHist[date]["CO2"]))
+        os.write(fDest, b'",\n\t\t\t"Activite" : "')
+        os.write(fDest, str.encode(donneesHist[date]["Activite"]))
+        os.write(fDest, b'",\n\t\t\t"Alerte" : "')
+        os.write(fDest, str.encode(donneesHist[date]["Alerte"]))
+        os.write(fDest, b'"\n\t\t}')
+        os.write(fDest, b'\n\t}')
+        os.write(fDest, b'\n}')
+        os.close(fDest)
 
     else:
-        existe = False
-        for cle, val in donnees_existantes.items():
-            if cle == nomSalle:
-                donnees_existantes[nomSalle].update(donneesHist)
-                existe = True
-                break
+        with open('historique.json', 'r') as fichier_json:
+            donnees_existantes = json.load(fichier_json)
 
-        if not existe:
+        if len(donnees_existantes) == 0:
             donneesNvSalle = {nomSalle: donneesHist}
             donnees_existantes.update(donneesNvSalle)
 
-    with open('historique.json', 'w') as fichier_json:
-        json.dump(donnees_existantes, fichier_json, indent=4)
+        else:
+            existe = False
+            for cle, val in donnees_existantes.items():
+                if cle == nomSalle:
+                    donnees_existantes[nomSalle].update(donneesHist)
+                    existe = True
+                    break
 
-    os.close(fDest)
+            if not existe:
+                donneesNvSalle = {nomSalle: donneesHist}
+                donnees_existantes.update(donneesNvSalle)
+
+        with open('historique.json', 'w') as fichier_json:
+            json.dump(donnees_existantes, fichier_json, indent=4)
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -52,20 +74,25 @@ def on_message(client, userdata, msg):
         co2 = data[0]["co2"]
         act = data[0]["activity"]
 
-        os.write(fDest, b'{\n')
-        os.write(fDest, b'\t"Salle" : "')
+        os.write(fDest, b'{\n\t"')
         os.write(fDest, str.encode(salle))
-        os.write(fDest, b'"')
-        os.write(fDest, b',\n\t"Temperature" : ')
-        os.write(fDest, str.encode(str(temp)))
-        os.write(fDest, b',\n\t"Humidite" : ')
+        os.write(fDest, b'": {\n')
+        os.write(fDest, b'\t\t"')
+        os.write(fDest, str.encode(datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")))
+        os.write(fDest, b'": {\n')
+        os.write(fDest, b'\t\t\t"Humidite": "')
         os.write(fDest, str.encode(str(hum)))
-        os.write(fDest, b',\n\t"CO2" : ')
+        os.write(fDest, b'",\n\t\t\t"CO2" : "')
         os.write(fDest, str.encode(str(co2)))
-        os.write(fDest, b',\n\t"Activite" : ')
+        os.write(fDest, b'",\n\t\t\t"Activite" : "')
         os.write(fDest, str.encode(str(act)))
+        os.write(fDest, b'",\n\t\t\t"Alerte" : "')
+        os.write(fDest, b'non')
+        os.write(fDest, b'"\n\t\t}')
+        os.write(fDest, b'\n\t}')
         os.write(fDest, b'\n}')
         os.close(fDest)
+
         donneesHist = {
             datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"): {
                 "Humidite": str(hum),
