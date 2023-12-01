30fc64b374cb88344cbe9a98734883ce4602082e
tristandlth <85364501+tristandlth@users.noreply.github.com>
2023-12-01 16:49:32 +0100

    Amélioration du visuel

---
+++ Site e-commerce/include/footer.php
@@ -0,0 +1,20 @@
+<footer class="footer">
+    <div class="footer-section">
+        <p class="small-text gray-text">Besoin d'aide ?</p>
+        <p class="large-text white-text">contact@sigmaprime.com</p>
+    </div>
+    <div class="footer-section">
+        <p class="small-text gray-text">Nous suivre</p>
+        <div class="logos-reseaux">
+            <!-- Il faudra ajouter les logo des réseaux plus tard, pour l'instant c'est simplement une maquette -->
+            <div class="logo-reseaux">1</div>
+            <div class="logo-reseaux">2</div>
+            <div class="logo-reseaux">3</div>
+            <div class="logo-reseaux">4</div>
+        </div>
+    </div>
+    <div class="footer-section third">
+        <p class="small-text gray-text">Paiement</p>
+        <p class="large-text white-text">CB, Paypal, Virement bancaire</p>
+    </div>
+</footer>
