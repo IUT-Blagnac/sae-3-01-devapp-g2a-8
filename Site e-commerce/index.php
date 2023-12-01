17ce1221e9c7d46e08c649ddb60a50e1b13adc69
tristandlth <85364501+tristandlth@users.noreply.github.com>
2023-12-01 11:36:00 +0100

    Charte graphique du site

---
+++ Site e-commerce/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>SigmaPrime</title>
+</head>
+<body>
+
+    <header>
+        <div id="header-left">
+            <a href="index.html">
+                <img src="img/SigmaPrime_Logo.png" alt="Logo SigmaPrime">
+            </a>
+            <nav>
+              <ul class="main-menu">
+                <li>
+                  <a href="#">Nutrition</a>
+                  <ul class="menu-deroulant">
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Compléments</a>
+                  <ul class="menu-deroulant">
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Vêtements</a>
+                  <ul class="menu-deroulant">
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Conseils sportifs</a>
+                  <ul class="menu-deroulant">
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                    <li><a href="#">Texte</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+        </div>
+        <div id="header-right">
+            <input type="text" placeholder="Rechercher...">
+            <button id="panier-button">Panier</button>
+            <button id="login-button">Connexion</button>
+        </div>
+    </header>
+
+    <p>...</p>
+
+    <footer class="footer">
+        <div class="footer-section">
+            <p class="small-text gray-text">Besoin d'aide ?</p>
+            <p class="large-text white-text">contact@sigmaprime.com</p>
+        </div>
+        <div class="footer-section">
+            <p class="small-text gray-text">Nous suivre</p>
+            <div class="logos-reseaux">
+                <!-- Il faudra ajouter les logo des réseaux plus tard, pour l'instant c'est simplement une maquette -->
+                <div class="logo-reseaux">1</div>
+                <div class="logo-reseaux">2</div>
+                <div class="logo-reseaux">3</div>
+                <div class="logo-reseaux">4</div>
+            </div>
+        </div>
+        <div class="footer-section third">
+            <p class="small-text gray-text">Paiement</p>
+            <p class="large-text white-text">CB, Paypal, Virement bancaire</p>
+        </div>
+    </footer>
+
+</body>
+</html>
